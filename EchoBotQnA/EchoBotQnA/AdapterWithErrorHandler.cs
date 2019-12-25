0d2e0fcbc36ec48f405876868862007b365064aa
Michael Miele <v-mimiel@microsoft.com>
2019-12-25 15:11:08 -0800

    Personal Notes

---
+++ EchoBotQnA/EchoBotQnA/AdapterWithErrorHandler.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace EchoBotQnA
+{
+    public class AdapterWithErrorHandler : BotFrameworkHttpAdapter
+    {
+        public AdapterWithErrorHandler(IConfiguration configuration, ILogger<BotFrameworkHttpAdapter> logger)
+            : base(configuration, logger)
+        {
+            OnTurnError = async (turnContext, exception) =>
+            {
+                // Log any leaked exception from the application.
+                logger.LogError($"Exception caught : {exception.Message}");
+
+                // Send a catch-all apology to the user.
+                string err = "Exception caught :" + exception.Message;
+                await turnContext.SendActivityAsync("Sorry, it looks like something went wrong. " + err);
+            };
+        }
+    }
+}
